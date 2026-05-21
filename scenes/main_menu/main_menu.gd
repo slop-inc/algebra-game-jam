06da9fc307b6a8156d8630354b68f1bb86e6205d
Zedorfska <zedorfska@gmail.com>
2026-05-21 15:20:59 +0200

    door

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,11 +1,13 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_start_pressed() -> void:
 	pass
