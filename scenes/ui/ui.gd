732e249080a74d7856c1decda764da89eb1a93fc
Zedorfska <zedorfska@gmail.com>
2026-05-22 15:52:23 +0200

    menu and other shit

---
+++ scenes/ui/ui.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+@onready var top_bar = $Control/TopBar
+@onready var bottom_bar = $Control/Bottombar
+var top_max = -420
+var bottom_max = 420
+
+# Pass 1.0 for full health
+# Pass 0.0 for death
+func set_bar_percentage(percentage: float) -> void:
+	var bottom_tween = get_tree().create_tween()
+	await bottom_tween.tween_property(bottom_bar, "position:y", bottom_max * percentage, 2)
+	var top_tween = get_tree().create_tween()
+	await top_tween.tween_property(top_bar, "position:y", top_max * percentage, 2)
+	
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
