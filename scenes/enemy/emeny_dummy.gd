72d99fb0de88f0ce639c4a34dd5a89b58f2a0c55
Luce <zedniklucijan5@gmail.com>
2026-05-22 18:16:58 +0200

    Enemy AI (They think sometimes)

--- scenes/enemy/emeny_dummy.gd
+++ scenes/enemy/emeny_dummy.gd
@@ -2,10 +2,15 @@ extends CharacterBody3D
 
 var max_health = 100
 var health = 100
-@onready var shit = $NavigationAgent3D
+var is_stunned = false
+var stun_time = 0
+
+var state_machine = self.get_child(1)
+
+signal GotKicked 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(shit)
+
 	health = max_health
 
 
@@ -13,7 +18,39 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if health <= 0:
 		print("died")
+		
+func _physics_process(delta: float) -> void:
+	if is_stunned:
+		if stun_time < 0.4:
+			stun_time += 1 * delta
+			print(is_stunned)
+		else:
+			stun_time = 0
+			is_stunned = false
+			print(is_stunned)
+			
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
 	
 func _takeDamage(dmg: int):
 	health = health - dmg
 	print("got hit")
+	
+func _getKicked(dmg: int, kick_direction: Vector3):
+	print("i got kicked")
+	health = health - dmg
+	is_stunned = true
+	print(is_stunned)
+	velocity.x = kick_direction.x * 15
+	velocity.z = kick_direction.z * 15
+	velocity.y = kick_direction.y * 15
+	
+
+
+func _on_instakill_detector_body_entered(body: Node3D) -> void:
+	var player = get_tree().get_first_node_in_group("player")
+	if body == player:
+		health = 0
+		
+		
