27e60df7c196363eec0a44d8f1dbea40cb435d22
Zedorfska <zedorfska@gmail.com>
2026-05-22 13:02:09 +0200

    audio for the door which probably doesnt work

--- scenes/building_blocks/door.gd
+++ scenes/building_blocks/door.gd
@@ -2,13 +2,20 @@ extends Node3D
 
 @onready var static_door = $StaticDoor
 @onready var physics_door = $PhysicsDoor
+@onready var sound_player = $PhysicsDoor/AudioStreamPlayer3D
+var door_kick_sounds_dir = "res://assets/sound/door_kick_sounds/"
 
 func _kick() -> void:
+	var sounds = [ ]
 	if static_door:
 		static_door.queue_free()
 	physics_door.visible = true
 	physics_door.freeze = false
 	physics_door.apply_force(Vector3(-500, 250, -600))
+	for sound in DirAccess.get_files_at(door_kick_sounds_dir):
+		sounds.append(sound)
+	sound_player.set_stream(door_kick_sounds_dir + sounds.pick_random())
+	sound_player.play()
 
 func _ready() -> void:
 	physics_door.freeze = true
