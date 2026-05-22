dabca394eaf4801b58e028292e819befb40a3da8
Zedorfska <zedorfska@gmail.com>
2026-05-22 17:17:16 +0200

    Door kick sounds

--- scenes/building_blocks/catacombs_door.gd
+++ scenes/building_blocks/catacombs_door.gd
@@ -4,20 +4,21 @@ extends Node3D
 @onready var physics_door = $PhysicsDoor
 @onready var sound_player = $PhysicsDoor/AudioStreamPlayer3D
 var door_kick_sounds_dir = "res://assets/sound/door_kick_sounds/"
+var kick_sounds = [ ]
 
 func _kick() -> void:
-	var sounds = [ ]
 	if static_door:
 		static_door.queue_free()
 	physics_door.visible = true
 	physics_door.freeze = false
 	physics_door.apply_force(Vector3(-500, 250, -600))
-	for sound in DirAccess.get_files_at(door_kick_sounds_dir):
-		sounds.append(sound)
-	sound_player.set_stream(door_kick_sounds_dir + sounds.pick_random())
+	sound_player.set_stream(kick_sounds.pick_random())
 	sound_player.play()
 
 func _ready() -> void:
+	for i in DirAccess.get_files_at(door_kick_sounds_dir):
+		if i.ends_with(".ogg"):
+			kick_sounds.append(load(door_kick_sounds_dir + i))
 	physics_door.freeze = true
 
 func _process(delta: float) -> void:
