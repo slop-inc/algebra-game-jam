89fb625c0fd68405c92eb65a1ced2cc635862e8e
LucijanZ <Zedorfska@gmail.com>
2026-05-23 11:24:27 +0200

    buh

--- scenes/enemy/EnemyDeath.gd
+++ scenes/enemy/EnemyDeath.gd
@@ -2,6 +2,7 @@ extends State
 class_name EnemyDeath
 @onready var anim = $"../../cultist"
 @onready var agent = self.get_parent().get_parent()
+@onready var player = get_tree().get_first_node_in_group("player")
 const blood_explosion = preload("res://scenes/enemy/BloodExplosion.tscn")
 
 
@@ -12,11 +13,15 @@ func Enter():
 	
 func _die():
 	if agent.explode:
+		if agent.is_boss:
+			player.end_game()
 		var blood_instance = blood_explosion.instantiate()
 		get_tree().root.get_child(0).add_child(blood_instance)
 		blood_instance.global_position = agent.global_position
 		agent.queue_free()
 	else:
+		if agent.is_boss:
+			player.end_game()
 		anim._die()
 		anim.reparent(get_tree().root.get_child(0))
 		agent.queue_free()
