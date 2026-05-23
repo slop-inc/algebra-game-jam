5dfe070b68a64734623cca01c087146007341e47
Zedorfska <zedorfska@gmail.com>
2026-05-23 09:56:31 +0200

    a

--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 @onready var top_bar = $Control/TopBar
 @onready var bottom_bar = $Control/Bottombar
+@onready var blood = $Control/DiddyBlud
 var top_max = -420
 var bottom_max = 420
 
@@ -12,7 +13,11 @@ func set_bar_percentage(percentage: float) -> void:
 	await bottom_tween.tween_property(bottom_bar, "position:y", bottom_max * percentage, 2)
 	var top_tween = get_tree().create_tween()
 	await top_tween.tween_property(top_bar, "position:y", top_max * percentage, 2)
-	
+
+func bloody():
+	blood.modulate.a = 0.75
+	var tween = get_tree().create_tween()
+	tween.tween_property(blood, "modulate:a", 0.0, 3)
 
 func _ready() -> void:
 	pass # Replace with function body.
